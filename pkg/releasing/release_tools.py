5abaf05ce63a22f55a6c65c481b51d13d8267276
fweikert <fwe@google.com>
2019-09-26 17:43:21 +0200

    WORKSPACE stanza: Do not include deps method by default
    Previously the WORKSPACE stanza would include a deps method with a default name if no explicit value was set by the rule.
    This caused problems for projects that don't require any dependencies to be loaded.
    
    This commit ensures that the WORKSPACE stanza only contains a load statement for the setup file if the caller sets an explicit deps or toolchains method.

--- pkg/releasing/release_tools.py
+++ pkg/releasing/release_tools.py
@@ -19,6 +19,24 @@ from string import Template
 import textwrap
 
 
+WORKSPACE_STANZA_TEMPLATE = Template(textwrap.dedent(
+    """
+    load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+    http_archive(
+        name = "${repo}",
+        url = "${url}",
+        sha256 = "${sha256}",
+    )
+    """).strip())
+
+
+DEPS_STANZA_TEMPLATE = Template(textwrap.dedent(
+    """
+    load("@${repo}//${setup_file}", ${to_load})
+    """).strip())
+
+
+
 def package_basename(repo, version):
   return '%s-%s.tar.gz' % (repo, version)
 
@@ -32,31 +50,26 @@ def get_package_sha256(tarball_path):
 def workspace_content(url, repo, sha256, setup_file=None, deps_method=None,
                       toolchains_method=None):
   # Create the WORKSPACE stanza needed for this rule set.
-  if not deps_method:
-    deps_method = repo + '_dependencies'
-  to_load = ['"%s"' % deps_method]
+  methods = []
+  if deps_method:
+    methods.append(deps_method)
   if toolchains_method:
-    to_load.append('"%s"' % toolchains_method)
+    methods.append(toolchains_method)
 
-  workspace_stanza_template = Template(textwrap.dedent(
-      """
-      load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-      http_archive(
-          name = "${repo}",
-          url = "${url}",
-          sha256 = "${sha256}",
-      )
-      load("@${repo}//${setup_file}", ${to_load})
-      ${deps_method}()
-      """).strip())
-  ret = workspace_stanza_template.substitute({
+  ret = WORKSPACE_STANZA_TEMPLATE.substitute({
       'url': url,
       'sha256': sha256,
       'repo': repo,
-      'setup_file': setup_file or ':deps.bzl',
-      'deps_method': deps_method,
-      'to_load': ', '.join(to_load),
   })
-  if toolchains_method:
-    ret += '\n%s()' % toolchains_method
+  if methods:
+    deps = DEPS_STANZA_TEMPLATE.substitute({
+        'repo': repo,
+        'setup_file': setup_file or ':deps.bzl',
+        'to_load': ', '.join('"%s"' % m for m in methods),
+    })
+    ret += "\n%s" % deps
+
+  for m in methods:
+    ret += '\n%s()' % m
+
   return ret
