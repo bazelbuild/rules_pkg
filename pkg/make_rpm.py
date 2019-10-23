58150a16a1585627af5b92b40931332cbaf97d18
mmikitka <matt@mikitka.net>
2019-10-23 19:49:56 -0400

    Changed the buildroot from BUILD to BUILDROOT

--- pkg/make_rpm.py
+++ pkg/make_rpm.py
@@ -152,6 +152,7 @@ class RpmBuilder(object):
 
   SOURCE_DIR = 'SOURCES'
   BUILD_DIR = 'BUILD'
+  BUILDROOT_DIR = 'BUILDROOT'
   TEMP_DIR = 'TMP'
   DIRS = [SOURCE_DIR, BUILD_DIR, TEMP_DIR]
 
@@ -209,7 +210,7 @@ class RpmBuilder(object):
   def CallRpmBuild(self, dirname):
     """Call rpmbuild with the correct arguments."""
 
-    buildroot = os.path.join(dirname, RpmBuilder.BUILD_DIR)
+    buildroot = os.path.join(dirname, RpmBuilder.BUILDROOT_DIR)
     args = [
         self.rpmbuild_path,
         '--define',
