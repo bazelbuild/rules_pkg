2af3c18e9cc341f3e2c72c7627f10ceb984a2606
Tony Aiuto <aiuto@google.com>
2020-07-02 01:33:31 -0400

    upd

--- pkg/tests/pkg_tar_test.py
+++ pkg/tests/pkg_tar_test.py
@@ -157,13 +157,6 @@ function assert_content() {
 }
 
 function test_tar() {
-  local listing="./
-./etc/
-./etc/nsswitch.conf
-./usr/
-./usr/titi
-./usr/bin/
-./usr/bin/java -> /path/to/bin/java"
   for i in "" ".gz" ".bz2" ".xz"; do
     assert_content "test-tar-${i:1}.tar$i"
     # Test merging tar files
