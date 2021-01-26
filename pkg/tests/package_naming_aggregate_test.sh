9f247feb09d12353f0bed17ad024bf9a584607a5
aiuto <aiuto@google.com>
2021-01-26 00:53:12 -0500

    forgot to add file

---
+++ pkg/tests/package_naming_aggregate_test.sh
@@ -0,0 +1,10 @@
+# This test simply checks that when we create packages with package_file_name
+# that we get the expected file names.
+set -e
+
+declare -r DATA_DIR="${TEST_SRCDIR}/rules_pkg/tests"
+
+for pkg in test_naming_some_value.tar test_naming_some_value.zip ; do
+  ls -l "${DATA_DIR}/$pkg"
+done
+echo "PASS"
