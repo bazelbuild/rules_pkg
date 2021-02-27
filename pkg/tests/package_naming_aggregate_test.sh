96875264f47d05d360c56a4ad626719921e5c2ca
Konstantin Erman <kerman@tableau.com>
2021-02-26 16:03:28 -0800

    Implemented package_variables substitution for package_dir attribute.

--- pkg/tests/package_naming_aggregate_test.sh
+++ pkg/tests/package_naming_aggregate_test.sh
@@ -21,4 +21,12 @@ declare -r DATA_DIR="${TEST_SRCDIR}/rules_pkg/tests"
 for pkg in test_naming_some_value.deb test_naming_some_value.tar test_naming_some_value.zip ; do
   ls -l "${DATA_DIR}/$pkg"
 done
+
+TAR_STRUCTURE=$(tar -tf tests/test_tar_package_dir_substitution.tar)
+EXPECTED_OUTPUT=$'./\x0a./level1/\x0a./level1/some_value/\x0a./level1/some_value/level3/\x0a./level1/some_value/level3/BUILD'
+if [[ "$TAR_STRUCTURE" != "$EXPECTED_OUTPUT" ]]; then
+    echo "Unexpected TAR structure."
+    exit 1
+fi
+
 echo "PASS"
