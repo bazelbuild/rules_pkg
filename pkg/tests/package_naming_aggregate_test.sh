bac5d0b08dae06ee08222378d1b291af49b227e7
Konstantin Erman <kerman@tableau.com>
2021-03-01 12:52:29 -0800

    Removed unnecessary empty line.

--- pkg/tests/package_naming_aggregate_test.sh
+++ pkg/tests/package_naming_aggregate_test.sh
@@ -21,5 +21,4 @@ declare -r DATA_DIR="${TEST_SRCDIR}/rules_pkg/tests"
 for pkg in test_naming_some_value.deb test_naming_some_value.tar test_naming_some_value.zip ; do
   ls -l "${DATA_DIR}/$pkg"
 done
-
 echo "PASS"
