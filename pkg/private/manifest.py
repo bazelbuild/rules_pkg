fb698a1307781469d14a31f15a9f31c8af62f573
Andrew Psaltis <apsaltis@vmware.com>
2021-07-14 18:29:55 -0400

    Fix spelling

--- pkg/private/manifest.py
+++ pkg/private/manifest.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Common package builder manfiest helpers
+"""Common package builder manifest helpers
 """
 
 import collections
