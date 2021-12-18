b98194f2a7295617a0d86c343b8a9ccc8b486497
Andrew Psaltis <apsaltis@vmware.com>
2021-12-18 14:07:49 -0500

    Begin migration stuff

--- migration/migrate_strip_prefix.py
+++ migration/migrate_strip_prefix.py
@@ -1,9 +1,58 @@
 #!/usr/bin/env python3
 
+import argparse
+import pathlib
+import sys
+
 import libcst as cst
 import libcst.matchers as cstm
 
 class PkgFilesStripPrefixTransformer(cstm.MatcherDecoratableTransformer):
-    pass
+    METADATA_DEPENDENCIES = (cst.metadata.PositionProvider,)
+    @cstm.call_if_inside(cstm.Call(
+        func = cstm.Name("pkg_files")
+    ))
+    @cstm.leave(cstm.Arg(keyword = cstm.Name("strip_prefix")))
+    def rename_strip_prefix_to_local_strip_prefix(self,
+                                                  original_node: cst.Arg,
+                                                  updated_node: cst.Arg) -> cst.Arg:
+        #print(self.metadata)
+        # pos = self.get_metadata(cst.metadata.PositionProvider, original_node).start
+        # print(dir(self.get_metadata(cst.metadata.PositionProvider, original_node).start))
+        # print(f"{updated_node.keyword.value} found at line {pos.line}, column {pos.column}")
+        return updated_node.with_changes(keyword = cst.Name("local_strip_prefix"))
 
+    # @cstm.call_if_inside(cstm.Call(
+    #     func = cstm.Name("pkg_files")
+    # ))
+    # @cstm.leave(cstm.Call(func = cstm.Name("pkg_files")))
+    # def yeah_whatever(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+    #     print(updated_node.args)
+    #     return updated_node
+    
+def main(argv):
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('-i', '--in-place', action='store_true', default=False)
+    parser.add_argument("paths", nargs='+', type=pathlib.Path, action='store')
+
+    args = parser.parse_args(argv)
+    for p in args.paths:
+        if p.is_dir():
+            # We're trying to modify a file in here -- it's called "BUILD.bazel"
+            p /= "BUILD"
+        text = p.open().read()
+        source_tree = cst.parse_module(text)
+        wrapper = cst.metadata.MetadataWrapper(source_tree)
+        transformer = PkgFilesStripPrefixTransformer()
+
+        new_code = wrapper.visit(transformer)
+        if args.in_place:
+            code = new_code.code
+            with p.open('w') as fh:
+                fh.write(code)
+        else:
+            print(new_code.code)
+
+if __name__ == '__main__':
+    exit(main(sys.argv[1:]))
