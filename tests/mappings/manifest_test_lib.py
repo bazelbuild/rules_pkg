2203d06c7df875bef8bdeda88db98b38f64a87b4
Tony Aiuto <aiuto@google.com>
2023-08-10 13:10:00 -0400

    restore badly merged test fix

--- tests/mappings/manifest_test_lib.py
+++ tests/mappings/manifest_test_lib.py
@@ -23,19 +23,47 @@ class ContentManifestTest(unittest.TestCase):
 
   run_files = runfiles.Create()
 
-  def assertManifestsMatch(self, expected, got):
+  def assertManifestsMatch(self, expected_path, got_path):
     """Check two manifest files for equality.
 
     Args:
-        expected: The path to the content we expect.
-        got: The path to the content we got.
+        expected_path: The path to the content we expect.
+        got_path: The path to the content we got.
     """
-    e_file = ContentManifestTest.run_files.Rlocation('rules_pkg/' + expected)
-    with open(e_file, mode='rb') as e_fp:
+    e_file = ContentManifestTest.run_files.Rlocation('rules_pkg/' + expected_path)
+    with open(e_file, mode='rt', encoding='utf-8') as e_fp:
       expected = json.load(e_fp)
-    expected_dict = {x["dest"]: x for x in expected}
-    g_file = ContentManifestTest.run_files.Rlocation('rules_pkg/' + got)
-    with open(g_file, mode='rb') as g_fp:
+    expected_dict = {x['dest']: x for x in expected}
+    g_file = ContentManifestTest.run_files.Rlocation('rules_pkg/' + got_path)
+    with open(g_file, mode='rt', encoding='utf-8') as g_fp:
       got = json.load(g_fp)
-    got_dict = {x["dest"]: x for x in got}
-    self.assertEqual(expected_dict, got_dict)
+    got_dict = {x['dest']: x for x in got}
+    print(got_dict)
+    # self.assertEqual(expected_dict, got_dict)
+
+    ok = True
+    expected_dests = set(expected_dict.keys())
+    got_dests = set(got_dict.keys())
+    for dest, what in expected_dict.items():
+      got = got_dict.get(dest)
+      if got:
+        self.assertDictEqual(what, got)
+      else:
+        print('Missing expected path "%s" in manifest' % dest)
+        ok = False
+    for dest, what in got_dict.items():
+      expected = expected_dict.get(dest)
+      if expected:
+        self.assertDictEqual(expected, what)
+      else:
+        print('Got unexpected path "%s" in manifest:' % dest, what)
+        ok = False
+
+    if not ok:
+      print('To update the golden file:')
+      print('  cp bazel-bin/%s %s' % (got_path, expected_path))
+      print('or')
+      print('============= snip ==========')
+      print(got_dict)
+      print('============= snip ==========')
+    self.assertTrue(ok)
