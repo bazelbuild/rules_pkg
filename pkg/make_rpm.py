3cb6dc424d32d41581d7806163b1584581dc0ee5
Nic <138107617+nicbadiu@users.noreply.github.com>
2025-02-24 12:31:09 +0200

    Add Almalinux 9.5 as a supported `MakeRpm` version

--- pkg/make_rpm.py
+++ pkg/make_rpm.py
@@ -375,10 +375,10 @@ class RpmBuilder(object):
       '--define', '_builddir %s/BUILD' % dirname,
     ]
 
-    if debuginfo_type in ["fedora40", "centos7", "centos9", "almalinux9.3"]:
+    if debuginfo_type in ["fedora40", "centos7", "centos9", "almalinux9.3", "almalinux9.5"]:
       args += ['--undefine', '_debugsource_packages']
 
-    if debuginfo_type in ["centos7", "centos9", "almalinux9.3"]:
+    if debuginfo_type in ["centos7", "centos9", "almalinux9.3", "almalinux9.5"]:
       args += ['--define', 'buildsubdir .']
 
     if debuginfo_type == "fedora40":
