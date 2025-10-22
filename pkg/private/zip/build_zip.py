0fa07e1471cc5b978f832268a5f0e6cb66c18e79
Adrian Imboden <adi@thingdust.com>
2025-10-22 13:58:54 +0000

    Implement pkg_install(symlink) also for pkg_zip

--- pkg/private/zip/build_zip.py
+++ pkg/private/zip/build_zip.py
@@ -182,6 +182,11 @@ class ZipWriter(object):
       # Set directory bits
       entry_info.external_attr |= (UNIX_SYMLINK_BIT << 16)
       self.zip_file.writestr(entry_info, src.encode('utf-8'))
+    elif entry_type == manifest.ENTRY_IS_RAW_LINK:
+      entry_info.compress_type = zipfile.ZIP_STORED
+      # Set directory bits
+      entry_info.external_attr |= (UNIX_SYMLINK_BIT << 16)
+      self.zip_file.writestr(entry_info, os.readlink(src).encode('utf-8'))
     elif entry_type == manifest.ENTRY_IS_TREE:
       self.add_tree(src, dst_path, mode)
     elif entry_type == manifest.ENTRY_IS_EMPTY_FILE:
