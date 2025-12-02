1123afccbdd8476a4a76ec4910a6e786ecd324c6
tonyaiuto <tony@aiuto.dev>
2025-12-02 00:04:19 -0500

    allow tags in the matrix

--- .github/workflows/checks.sh
+++ .github/workflows/checks.sh
@@ -8,7 +8,12 @@ if [ "${exit_code}" -ne 0 ] ; then
     exit "${exit_code}"
 fi
 
-bazel test -- //tests/... //examples/... -//tests/rpm/...
+FILTERS=""
+if [[ "${TEST_FILTER:-}" ]] ; then
+   FILTERS=(--build_tag_filters="${TEST_FILTER}"  --test_tag_filters="${TEST_FILTER}")
+fi
+
+bazel test -- //tests/... //examples/... -//tests/rpm/... "${FILTERS[@]}"
 exit_code="$?"
 case "${exit_code}" in
   "4")
