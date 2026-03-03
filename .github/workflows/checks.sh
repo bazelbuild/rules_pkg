b3b1ad91b2a5559a6aeaaef28faf8979a37e5259
tonyaiuto <tony@aiuto.dev>
2026-03-02 21:53:47 -0500

    make it cleaner

--- .github/workflows/checks.sh
+++ .github/workflows/checks.sh
@@ -6,10 +6,6 @@ set -o errexit -o nounset -o pipefail
 bazel_cmd=(bazel query :all)
 echo "${bazel_cmd[@]}"
 ( cd providers ; "${bazel_cmd[@]}" )
-exit_code="$?"
-if [ "${exit_code}" -ne 0 ] ; then
-    exit "${exit_code}"
-fi
 
 FILTERS=()
 if [[ -n "${TEST_FILTER:-}" ]] ; then
@@ -22,18 +18,9 @@ bazel_cmd=(bazel test --build_tests_only "${FILTERS[@]}" -- ${TESTS} //examples/
 
 echo "${bazel_cmd[@]}"
 "${bazel_cmd[@]}"
-exit_code="$?"
-if [ "${exit_code}" -ne 0 ] ; then
-    exit "${exit_code}"
-fi
 
 if [ -n "${BUILD_DISTRO:-}" ] ; then
     bazel build  //distro:distro
-    exit_code="$?"
-    if [ "${exit_code}" -ne 0 ] ; then
-        echo "Could not build //distro:distro"
-        exit "${exit_code}"
-    fi
 fi
 
 exit "${exit_code}"
